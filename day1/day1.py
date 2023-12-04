93014d27da88b0ea94aff170f0290c801f281b75
Nono <noah.johanson@gmail.com>
2023-12-03 17:44:56 -0800

    add input.txt and correct solution

--- day1/day1.py
+++ day1/day1.py
@@ -1,27 +1,20 @@
 import re
 
-input = """1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet"""
+with open('input.txt') as f:
+    input = list(f)
 
-def split(s):
-    return s.split('\n') 
-
-ex_list = split(input)
+ex_list = input
 print(ex_list)
 sol = []
 n = 0
 
-
 while n < len(ex_list):
     item = re.sub(r'\D', '', ex_list[n])
-    if int(item) > 2:
-        item = item[0] + item[-1]
+    item = item[0] + item[-1]
     sol.append(int(item))
     n = n+1
 
-
 total = sum(sol)
-print(sol)
 print(total)
+print(sol)
+print(len(ex_list))
